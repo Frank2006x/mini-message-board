371273119313bc2ef9c2611c05573673ecd813bf
Frank2006x <frank2006x@gmail.com>
2025-05-23 15:14:16 +0530

    order by date

--- controllers/default.js
+++ controllers/default.js
@@ -1,8 +1,7 @@
-
-const db=require("../db/queries")
+const db = require("../db/queries");
 module.exports = {
   get: async (req, res) => {
-    const messages=await db.getAllMsg();
+    const messages = (await db.getAllMsg()).reverse();
     res.render("index", { msg: messages });
   },
 };
